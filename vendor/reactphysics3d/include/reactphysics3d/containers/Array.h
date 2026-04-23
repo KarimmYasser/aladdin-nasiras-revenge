efca504177123690f1b6a4ea09367e01a434c715
Karim Yasser <karimmyasserr@gmail.com>
2026-04-23 05:43:33 +0200

    chore: Apply compatibility fixes to ReactPhysics3D library containers and collision logic

--- vendor/reactphysics3d/include/reactphysics3d/containers/Array.h
+++ vendor/reactphysics3d/include/reactphysics3d/containers/Array.h
@@ -252,7 +252,7 @@ class Array {
             if (capacity <= mCapacity) return;
 
             // Make sure capacity is an integral multiple of alignment
-            capacity = std::ceil(capacity / float(GLOBAL_ALIGNMENT)) * GLOBAL_ALIGNMENT;
+            capacity = static_cast<uint64>(std::ceil(capacity / float(GLOBAL_ALIGNMENT)) * GLOBAL_ALIGNMENT);
 
             // Allocate memory for the new array
             void* newMemory = mAllocator.allocate(capacity * sizeof(T));
